fd4607a9d1c92e6c280de18a5a8facf2a7b7246a
Nicholas Tay <nexerq@gmail.com>
2017-02-04 09:21:01 +1100

    Update example to use promises & client id

--- examples/getChannelStreams.js
+++ examples/getChannelStreams.js
@@ -1,9 +1,13 @@
-var twitchStreams = require('../');
+var twitchStreams = require('../')('<client-id here - can be obtained from your twitch.tv settings -> connections -> register developer app>');
 
-twitchStreams.get('Monstercat', function(error, streams) {
-    if (error) return console.log('Error caught:', error);
-    console.log('Got stream data.');
-    for (var stream of streams) {
-        console.log(stream.quality + ' (' + stream.resolution + '): ' + stream.url);
-    }
-})
+twitchStreams.get('Monstercat')
+    .then(function(streams) {
+        console.log('Got stream data.');
+
+        for (var stream of streams)
+            console.log(stream.quality + ' (' + stream.resolution + '): ' + stream.url);
+    })
+    .catch(function(error) {
+        if (error)
+            return console.log('Error caught:', error);
+    });
